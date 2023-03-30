fe519f254f0ebf96dd3ff3986ecba7632571630d
AAydan <aydansa@code.az>
2023-03-29 19:38:58 -0700

    Js3

---
+++ assets/move.js
@@ -0,0 +1,31 @@
+let inp = document.getElementById('inp');
+let addBtn = document.getElementById('btn-add');
+let removeBtn = document.getElementById('btn-remove')
+let list = document.getElementById('list');
+let todo = [];
+
+function additems(){
+    if ((inp.value != '') && (!todo.includes(`${inp.value}`))) {
+        todo.push(inp.value);
+    }
+
+    let data = '';
+    for(let item of todo){
+        list.style =
+        data += `<li class="list-group-item"> ${item} </li>` 
+    }   
+    inp.value = '';
+    list.innerHTML = data;
+}
+
+function removeItems(){
+    if (todo.length > 0) {
+        todo.pop(todo[0]);
+      }
+    
+    if (list.children.length > 0) {
+        list.removeChild(list.lastChild);
+      }
+    
+}
+
